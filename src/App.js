33b89079ea0f3119fd4a53c33fda587fd80dc054
Kyle Welch <kywelch17@gmail.com>
2020-12-23 10:38:43 -0500

    footer added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import FooterContainer from './containers/footer';
 import JumbotronContainer from './containers/jumbotron';
 
 export default function App() {
     return (
-        <JumbotronContainer />
+        <><JumbotronContainer /><FooterContainer /></>
     )
 }
---
+++ src/containers/footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Footer from '../components/footer';
+
+export default function FooterContainer() {
+    return (
+        <Footer>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href='#'>FAQ</Footer.Link>
+                    <Footer.Link href='#'>Investors</Footer.Link>
+                    <Footer.Link href='#'>Ways to Watch</Footer.Link>
+                    <Footer.Link href='#'>Originals</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href='#'>FAQ</Footer.Link>
+                    <Footer.Link href='#'>Investors</Footer.Link>
+                    <Footer.Link href='#'>Ways to Watch</Footer.Link>
+                    <Footer.Link href='#'>Originals</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href='#'>FAQ</Footer.Link>
+                    <Footer.Link href='#'>Investors</Footer.Link>
+                    <Footer.Link href='#'>Ways to Watch</Footer.Link>
+                    <Footer.Link href='#'>Originals</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href='#'>FAQ</Footer.Link>
+                    <Footer.Link href='#'>Investors</Footer.Link>
+                    <Footer.Link href='#'>Ways to Watch</Footer.Link>
+                    <Footer.Link href='#'>Originals</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+        </Footer>
+    )
+}
